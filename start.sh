f4fa1b9035c5e1b03b5b81613e800ff9ad32eab2
Lou Lindsley <4loubird@gmail.com>
2025-09-17 12:39:04 -0400

    Fixed the start.sh so it opens Safari after container is running fully

--- start.sh
+++ start.sh
@@ -1,23 +1,24 @@
-# starts r studio and 
+# starts rocker/verse container and opens Safari to localhost:8787
 
-
+echo "Opening Docker..."
 cd ~
-echo "Opening Docker"
 open -a Docker
+
+# Wait until Docker is running
 sleep 2
 
-echo "Starting rocker/verse container"
-docker run -e USERID=$(id -u) -e GROUPID=$(id -g)\
+echo "Starting rocker/verse container in background..."
+cd ~/Project611
+docker run -d -e USERID=$(id -u) -e GROUPID=$(id -g)\
   --platform=linux/amd64\
   -v $(pwd):/home/rstudio/work\
   -v $HOME/.ssh:/home/rstudio/.ssh\
   -v $HOME/.gitconfig:/home/rstudio/.gitconfig\
   -e PASSWORD=123\
   -p 8787:8787 rocker/verse
-sleep 2
 
-echo "Opening localhost:8787"
-echo "Password: 123"
-open -a Safari localhost:8787
-
-setwd("~/work")
+echo "Opening localhost:8787..."
+cd ~
+# Wait until Container is running
+sleep 4
+open -a Safari http://localhost:8787
