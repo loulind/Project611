8f266a8964d1080e928789c5118d0d75d1d464e2
Lou Lindsley <4loubird@gmail.com>
2025-11-25 15:32:18 -0500

    builds the container with embedding model

--- start.sh
+++ start.sh
@@ -16,6 +16,6 @@ docker run\
   --platform=linux/amd64\
   -v $(pwd):/home/rstudio/work\
   -v $HOME/.ssh:/home/rstudio/.ssh\
-  -v $HOME/.gitconfig:/home/rstudio/.gitconfig\
   -e PASSWORD=123\
+  --add-host=host.docker.internal:host-gateway\
   -p 8787:8787 -it lou-final-project
