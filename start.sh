5d45382277b95612b5254a055ca63d82ab528456
Lou Lindsley <4loubird@gmail.com>
2025-12-01 11:43:46 -0500

    made script applicable to other unix-like platforms

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
-# starts rocker/verse container and opens Safari to localhost:8787
-
-echo "Opening Docker and Safari..."
-cd ~
-open -a Docker
-open -a Safari http://localhost:8787
-
-# Wait until Docker is running
-sleep 4
-
-echo "Starting Docker container..."
-cd ~/Project611
-
 docker build . --platform=linux/amd64 -t lou-final-project
 docker run\
   --platform=linux/amd64\
