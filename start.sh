d37e6a84a7e519aa05281578798b8919481ce7e0
Lou Lindsley <4loubird@gmail.com>
2025-12-03 23:06:25 -0500

    added porting for shiny

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 docker build . --platform=linux/amd64 -t lou-final-project
-docker run\
-  --platform=linux/amd64\
-  -v $(pwd):/home/rstudio/work\
-  -v $HOME/.ssh:/home/rstudio/.ssh\
-  -e PASSWORD=123\
-  --add-host=host.docker.internal:host-gateway\
-  -p 8787:8787 -it lou-final-project
+
+# ports to 8787 for rocker container and 6052 for shiny interactive output
+docker run \
+  --platform=linux/amd64 \
+  -v $(pwd):/home/rstudio/work \
+  -v $HOME/.ssh:/home/rstudio/.ssh \
+  -e PASSWORD=123 \
+  --add-host=host.docker.internal:host-gateway \
+  -p 8787:8787 \
+  -p 6052:6052 \
+  -it lou-final-project
